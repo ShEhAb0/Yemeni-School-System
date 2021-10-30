0554bd46a0471eece7bae2de1063a6666dbee03e
AlazaziAmr <alazaziamr@gmail.com>
2021-10-30 22:24:34 +0300

    Make Parent Choose Student, Complete Parent Schedule.

--- app/Http/Controllers/Parent/ParentController.php
+++ app/Http/Controllers/Parent/ParentController.php
@@ -10,22 +10,115 @@ use Illuminate\Support\Facades\Auth;
 
 class ParentController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-
-        //$parents_us = UserParent::where('parent_id',Auth::id())->with('user')->get();
-       // $parents_us = UserParent::with('user');
-        $parents_us = UserParent::where('parent_id',Auth::id())->with('user')->get();
-        //$user = User::all();
-
-
-        return view('pages.parent.index' , compact('parents_us'));
+        $student_id = session('student_id');
+        if($student_id) {
+            //
+//            $parents_us = UserParent::where('parent_id', Auth::id())->with('user')->get();
+            return view('pages.parent.index');
+        }else{
+            return redirect('/parent/choose-student');
+        }
     }
 
+    public function chooseStudent(Request $request){
+        $request->validate([
+            'student'=> 'required'
+        ]);
+        $student = User::find($request->student);
+        session()->put('student_id',$student->id);
+        session()->put('student_name',$student->student_name);
+        session()->put('student_username',$student->username);
+        session()->put('student_image',$student->image);
+        session()->put('student_level',$student->level_id);
+        session()->put('student_term',$student->term_id);
+        return redirect('/parent/index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        if (session('student_id')) {
+            session()->remove('student_id');
+            session()->remove('student_name');
+            session()->remove('student_username');
+            session()->remove('student_image');
+            session()->remove('student_level');
+            session()->remove('student_term');
+            $parents_us = UserParent::where('parent_id', Auth::id())->with('user')->get();
+            return view('pages.parent.selectStudent', compact('parents_us'));
+        }else{
+            $parents_us = UserParent::where('parent_id', Auth::id())->with('user')->get();
+            return view('pages.parent.selectStudent', compact('parents_us'));
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show($id)
     {
+        //
         $parents_us = UserParent::where('id' , $id)->first();
         return view('pages.parent.show' , compact('parents_us'));
+    }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
